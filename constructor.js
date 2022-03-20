ab3fc118b49354bff8f4d984d8e0e3cb3d9027d3
kadekframa <dframa19@gmail.com>
2022-03-20 16:18:36 +0800

    ES6 JavaScript. contoh constructor pada OOP JavaScript. Keep spirit !

---
+++ constructor.js
@@ -0,0 +1,8 @@
+
+class Car{
+    constructor(manufacture, color){        // contoh dari constructor javascript.
+        this.manufacture = manufacture;
+        this.color = color;
+        this.enginesActive = false;
+    }
+}
