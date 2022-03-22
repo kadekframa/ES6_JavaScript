2ed296293e145d9d07cd14550086491edd2c5bd7
kadekframa <dframa19@gmail.com>
2022-03-22 22:47:17 +0800

    ES6 JavaScript. Penggunaan .then() untuk menampilkan nilai dari parameter resolve (onFulfilled) dan reject (onRejected) pada OOP JavaScript. Keep spirit ! dan Tetap Semangat !!!

--- promise.js
+++ promise.js
@@ -3,8 +3,31 @@
 
 // const coffee = new Promise();       // membuat object promise. untuk membuat objek promise, kita gunakan kata kunci new dan diikuti dengan constructor dari Promise.
 
+// const executorFunction = (resolve, reject) => {
+//     const isCoffeeMakerReady = true;
+//     if(isCoffeeMakerReady){
+//         resolve("Kopi Berhasil Dibuat");
+//     }else{
+//         reject("Mesin Kopi Tidak bisa Digunakan!");
+//     }
+// }
+
+// const handlerSuccess = resolvedValue => {
+//     console.info(resolvedValue);
+// }
+
+// const makeCoffee = new Promise(executorFunction);
+// makeCoffee.then(handlerSuccess);    // Untuk menangani nilai yang dikirimkan oleh resolve() ketika promise onFulfilled, kita gunakan method .then() pada objek promise yang kita buat.
+                                    // Lalu di dalam method .then() kita berikan parameter berupa function yang berguna sebagai handle callback.
+
+// Namun bagaimana jika objek promise menghasilkan kondisi rejected? Bagaimana cara menangani nilai yagn dikirimkan oleh reject() ?
+/*  Kita dapat menyimpan kedua handle callback baik onFulfilled atau onRejected pada parameter method .then(),
+    yang perlu kita perhatikan adalah pastikan handle callback untuk onFulfilled disimpan pada parameter pertama
+    seperti ini:
+*/
+
 const executorFunction = (resolve, reject) => {
-    const isCoffeeMakerReady = false;
+    const isCoffeeMakerReady = true;
     if(isCoffeeMakerReady){
         resolve("Kopi Berhasil Dibuat");
     }else{
@@ -12,5 +35,14 @@ const executorFunction = (resolve, reject) => {
     }
 }
 
+const handlerSuccess = resolvedValue => {
+    console.info(resolvedValue);
+}
+
+const handlerRejected = rejectionReason => {
+    console.info(rejectionReason);
+}
+
+
 const makeCoffee = new Promise(executorFunction);
-console.info(makeCoffee);
+makeCoffee.then(handlerSuccess, handlerRejected);       // penempatan paramter onFulfilled dan onRejected sebagai parameter pada .then().
